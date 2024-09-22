6a06947dc4ee619bba1be30b5fd4bdc67cfe743c
MichelleL <michelleloi885@gmail.com>
2024-09-21 23:19:46 -0600

    FIX: remove tank svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import Fish from "./Fish";
 import { FishTank } from "./FishTank";
-import Tank from "./Tank.jsx";
 
 export default function App() {
     return(
@@ -11,10 +10,6 @@ export default function App() {
             </div>
             <div className="scene__tank">
                 Tank
-                <div>
-                    <Tank />
-
-                </div>
             </div>
         </div>
     )
