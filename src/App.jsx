90ef9b0b5ce6b91293a97e465984144bc1ba3661
MichelleL <michelleloi885@gmail.com>
2024-09-21 23:56:55 -0600

    FEAT: add components directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Fish from "./Fish";
 import { FishTank } from "./FishTank";
+import Feeder from "./components/feeder/Feeder.jsx";
 
 export default function App() {
     return(
         <div className="scene">
             <div className="scene__feeder">
-                Feeder
+                <Feeder />
             </div>
             <div className="scene__tank">
                 <FishTank/>
