88ce159bd4ca1caa4ea10e7040fde976e1b362ab
MichelleL <michelleloi885@gmail.com>
2024-09-21 23:09:03 -0600

    FEAT: added layout and tank

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
 import Fish from "./Fish";
 import { FishTank } from "./FishTank";
+import Tank from "./Tank.jsx";
 
 export default function App() {
     return(
-        <div className="background">
-            <FishTank/>
+        <div className="scene">
+            <div className="scene__feeder">
+                Feeder
+            </div>
+            <div className="scene__tank">
+                Tank
+                <div>
+                    <Tank />
+
+                </div>
+            </div>
         </div>
     )
 }
