27838e932a5abdfea5bba473bd493e53c6458eb7
Anh Nguyen <anhnguyen6535@gmail.com>
2024-09-24 12:00:26 -0600

    avoid fish spawn at the close locations

--- src/components/fish/FishTank.jsx
+++ src/components/fish/FishTank.jsx
@@ -1,24 +1,40 @@
 import { useState, useEffect } from 'react';
 import Fish from './Fish';
 
-const minDistance = 50;
+const minDistance = 100;
 
-const createRandomFish = (id) => ({
-  id,
-  top: Math.floor(Math.random() * 55) + 20,
-  left: Math.floor(Math.random() * 1000) + 400,
-  isColliding: false,
-});
+const createRandomFish = () => {
+  const newFishes = []
+  
+  for (let i =0; i < 5; i++){
+    let top, left
+    let isTooClose
+    do{
+      left = Math.floor(Math.random() * 1000) + 400
+      top = Math.floor(Math.random() * 55) + 20
+  
+      //check distance
+      isTooClose = newFishes.some(fish =>{
+        const distance = Math.sqrt(Math.pow(left - fish.left, 2) + Math.pow(top - fish.top, 2));
+        
+        return distance < minDistance 
+      })
+    }while(isTooClose)
+
+
+    newFishes.push({id: i, top, left, isColliding: false})
+  }
+  
+  return newFishes
+};
 
 export const FishTank = ({fishes, setFishes}) => {
   // const [fishes, setFishes] = useState([]);
 
   // Generate fish positions dynamically
   useEffect(() => {
-    const newFishes = Array.from({ length: 10 }, (_, index) =>
-      createRandomFish(index)
-    );
-    setFishes(newFishes);
+    const newFishes = createRandomFish()
+    setFishes(newFishes)
   }, []);
 
   return (
