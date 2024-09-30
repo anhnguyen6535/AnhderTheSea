562449457cfff98b1db3becc4680b379fc810eb1
MichelleL <michelleloi885@gmail.com>
2024-09-29 23:03:35 -0600

    CHORE: Change sound effect when eating

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Paw from "./components/paw/Paw.jsx";
 import {useEffect, useRef, useState} from "react";
 import bgMusic from "../public/happy-day-in-beach-hand-panwav-14755.mp3"
 import { useAudio } from "./hooks/useAudio.js";
-import popSound from "../public/ui-pop-up-1-197886.mp3"
 import videoBG from "../public/background3.mp4"
 import hitSound from "../public/angry-cat-hq-sound-effect-2406752.mp3"
+import eatSound from "../public/level-up-191997.mp3"
 
 export default function App() {
     const [hitAud] = useState(() => new Audio(hitSound))
@@ -67,8 +67,8 @@ export default function App() {
                                 newFoods.splice(i, 1); // remove the food right away so it doesn't collide with another fish
                                 setFoods(newFoods);
 
-                                // Fish eat food with a pop sound
-                                const shakeEffect = new Audio(popSound).play();
+                                // Fish eat food with a sound
+                                const eatEffect = new Audio(eatSound).play();
 
                                 // no need to check the other food for this fish it has eaten
                                 break;
